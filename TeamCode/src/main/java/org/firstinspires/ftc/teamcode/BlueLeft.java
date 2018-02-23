aff7f29f509bfdd66019fdd5a8a2b524ce9c8ed5
Sebastian McMillan <sebastian.mcmillan@gmail.com>
2018-02-23 18:13:36 -0500

    wot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLeft.java
@@ -200,7 +200,7 @@ public class BlueLeft extends LinearOpMode {
             telemetry.addData("Hue", hsvValues[0]);
 
             // change the background color to match the color detected by the RGB sensor.
-            // pass a reference to the hue, saturation, and value array as an argument
+            // pass a reference to the  hue, saturation, and value array as an argument
             // to the HSVToColor method.
             relativeLayout.post(new Runnable() {
                 public void run() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetzerAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetzerAuton.java
@@ -113,7 +113,7 @@ public class SetzerAuton extends LinearOpMode
         boolean red = false;
         boolean blue = false;
         boolean neither = true;
-        
+
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
